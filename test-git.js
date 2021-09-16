035c0411a6b048c61948acf57cfbd0c870bfacf9
Grant Williams <grant.williams2986@gmail.com>
2021-09-16 20:51:44 +0200

    try again

--- test-git.js
+++ test-git.js
@@ -3,27 +3,24 @@ const { promisify } = require('util');
 
 const execPromisified = promisify(childProcess.exec);
 const exec = async command => {
-  try {
-    const { stdout } = await execPromisified(command);
-    console.log('ok');
-    return stdout.trim();
-  } catch ({ stdout }) {
-    console.log('not ok');
-    return stdout.trim();
-  }
+  const output = await execPromisified(command)
+    .then(res => res.stdout.trim())
+    .catch(err => err.stderr.trim());
+  console.log(JSON.stringify(output, null, 2));
+  return output;
 };
 
 (async () => {
   const fetchOutput = await exec('git fetch origin my-new-branch');
 
+  console.log(fetchOutput);
   if (fetchOutput.indexOf("fatal: couldn't find remote")) {
     console.log('No remote');
     process.exit(0);
   }
 
-  await exec('git checkout my-new-branch');
+  await exec('git checkout --track origin/my-new-branch');
+  await exec('git branch -vv');
   await exec('git pull');
   await exec('git log');
-
-  console.log(fetchOutput);
 })();
